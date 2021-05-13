5dbb953ac8bdb9bb0cf0df880e8a65a130a7f6ca
dantereto <2247@holbertonschool.com>
2021-05-13 17:09:07 +0000

    betty

--- main.c
+++ main.c
@@ -4,48 +4,54 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * get_op_func - search the sign
- *@s: the sign
+ * main - main
+ *@argv: argv
+ *@argc: argc
  *Return: reyturn 0
  */
 int main(int argc, char *argv[])
 {
-  FILE *file;
-  char *buff = NULL;
-  size_t lon;
-  char *point = '\0';
-  unsigned int line = 0;
-  stack_t *stack = NULL;
-  void (*var_name)(stack_t **stack, unsigned int line_number) = NULL;
-  if (argc != 2)
-    {
-      fprintf(stderr, "USAGE: monty file\n");
-      exit(EXIT_FAILURE);
-    }
-  file = fopen(argv[1], "r");
-  if (!file)
-    {
-      fprintf(stderr, "Error: Can't open file file %s\n", argv[1]);
-      exit(EXIT_FAILURE);
-    }
-  while(getline(&buff, &lon, file) != EOF)
-    {
-      buff[strlen(buff) - 1] = '\0';
-      line++;
-      point = strtok(buff, "\n\t ");
-      var_name = get(point, line);
-      var_name(&stack, line);
-    }
-  hola(stack);
-  free(buff);
-  fclose(file);
-  exit(EXIT_SUCCESS);
-}
-void hola(stack_t *stack)
+FILE *file;
+char *buff = NULL;
+size_t lon;
+char *point = '\0';
+unsigned int line = 0;
+stack_t *stack = NULL;
+void (*var_name)(stack_t **stack, unsigned int line_number) = NULL;
+if (argc != 2)
 {
-  if (stack)
-    {
-      hola(stack->next);
-      free(stack);
-    }
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
+file = fopen(argv[1], "r");
+if (!file)
+{
+fprintf(stderr, "Error: Can't open file file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
+while (getline(&buff, &lon, file) != EOF)
+{
+buff[strlen(buff) - 1] = '\0';
+line++;
+point = strtok(buff, "\n\t ");
+var_name = get(point, line);
+var_name(&stack, line);
+}
+free_s(stack);
+free(buff);
+fclose(file);
+exit(EXIT_SUCCESS);
+}
+/**
+ * free_s - free the stack
+ *@stack: the stack
+ *Return: reyturn 0
+ */
+void free_s(stack_t *stack)
+{
+if (stack)
+{
+free_s(stack->next);
+free(stack);
+}
 }
