dafa74fb8505ff7bc91828ae7d450ea7fcd5e191
dantereto <2247@holbertonschool.com>
2021-05-13 17:04:33 +0000

    betty

--- add.c
+++ add.c
@@ -2,26 +2,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * clear_bit - set the bit on 0 to index
- *@n: the number
- *@index: the index
+ * _add - add element of the stack
+ *@stack: the stack
+ *@line: the line of error
  *Return: reyturn 0
  *stack_t **stack, unsigned int line
  */
-void _add(stack_t **stack, unsigned int len)
+void _add(stack_t **stack, unsigned int line)
 {
-  int sum, num;
-  num = _len(*stack);
-  if (num >= 2)
-    {
-      sum = (*stack)->n;
-      sum += (*stack)->next->n;
-      _pop(stack, len);
-      (*stack)->n = sum;
-    }
+int sum = 0, num;
+num = _len(*stack);
+if (num >= 2)
+{
+sum = (*stack)->n;
+sum += (*stack)->next->n;
+_pop(stack, line);
+(*stack)->n = sum;
 }
-void _nop(stack_t **stack, unsigned int len)
+}
+/**
+ * _nop - nop
+ *@stack: the stack
+ *@line: the line of error
+ *Return: reyturn 0
+ */
+void _nop(stack_t **stack, unsigned int line)
 {
-  (void)stack;
-  (void)len;
+(void)stack;
+(void)line;
 }
--- get_fun.c
+++ get_fun.c
@@ -3,29 +3,30 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *
+ * get_func - get the fununcion
+ *@head: head
+ *@n:the element
  * Return: Always EXIT_SUCCESS.
  */
 stack_t *get_func(stack_t **head, char *n)
 {
-  stack_t *new;
-  new = malloc(sizeof(stack_t));
-  if (!new)
-    {
-      dprintf(2, "Error: malloc failed\n");
-      free(new);
-      exit(EXIT_FAILURE);
-    }
-  new->n = atoi(n);
-  new->prev = NULL;
-  if (!(*head))
-    new->next = NULL;
-  else
-    {
-      new->next = *head;
-      (*head)->prev = new;
-    }
-  *head = new;
-  return(new);
+stack_t *new;
+new = malloc(sizeof(stack_t));
+if (!new)
+{
+dprintf(2, "Error: malloc failed\n");
+free(new);
+exit(EXIT_FAILURE);
+}
+new->n = atoi(n);
+new->prev = NULL;
+if (!(*head))
+new->next = NULL;
+else
+{
+new->next = *head;
+(*head)->prev = new;
+}
+*head = new;
+return (new);
 }
